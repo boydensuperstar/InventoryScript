0d79b48db4b60f7cadf4e76fee94e8de2c6b0469
Jeremy B. Warner <Jeremybwarner@gmail.com>
2019-12-01 19:46:02 -0800

    Create Inventory.cs

---
+++ Inventory.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Inventory : MonoBehaviour
+{
+
+    #region Singleton
+
+    public static Inventory instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("More than one instance of Inventory found!");
+            return;
+        }
+        instance = this;
+    }
+
+    #endregion
+
+    public delegate void OnItemChanged();
+    public OnItemChanged onItemChangedCallback;
+    public int ItemsCollected;
+    public int TotalItemCount;
+    public string FinalScene;
+
+    public int space = 20;
+
+    public List<Item> items = new List<Item>();
+
+    public bool Add (Item item)
+    {
+        if (!item.isDefaultItem)
+        {
+            if (items.Count >= space)
+            {
+                Debug.Log("Not Enough Room.");
+                return false;
+            }
+            items.Add(item);
+            ItemsCollected = ItemsCollected + 1;
+            Debug.Log("Items Collected = " + ItemsCollected);
+
+            if (ItemsCollected >= TotalItemCount)
+            {
+                SceneManager.LoadScene(FinalScene);
+                Debug.Log("Loading Final Scene");
+            }
+
+            if (onItemChangedCallback != null)
+            onItemChangedCallback.Invoke();
+        }
+
+        return true;
+
+    }
+
+    public void Remove(Item item)
+    {
+        items.Remove(item);
+
+        if (onItemChangedCallback != null)
+            onItemChangedCallback.Invoke();
+    }
+    
+}
